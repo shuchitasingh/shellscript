1b0cd7a565e4fdd466b46e3b235b1408f9094ec0
shuchitasingh <singhshuchita5@gmail.com>
2021-03-19 17:53:37 +0530

    employee WageForMonth

--- shellscriptProgram.sh
+++ shellscriptProgram.sh
@@ -58,3 +58,33 @@ case $randomCheck in
 		;;
 	*)
 esac
+
+
+
+WAGE_PER_HR=20
+ISFULLTIME=1
+ISPARTTIME=2
+WORKINGDAYS=20
+
+#VARIABLE
+totalSalary=0
+
+#CHECKS DAILYWAGE FOR FULLTIME AND PARTTIME USING CASE
+for (( day=1; day<=$WORKINGDAYS; day++ ))
+do
+	randomCheck=$((RANDOM%3))
+	case $randomCheck in
+		$ISFULLTIME)
+			empHrs=8
+			;;
+		$ISPARTTIME)
+			empHrs=4
+			;;
+		*)
+		empHrs=0
+			;;
+	esac
+salary=$(($empHrs*$WAGE_PER_HR))
+totalSalary=$(($totalSalary+$salary))
+done
+echo "Total salary of an employee in month is $totalSalary"
