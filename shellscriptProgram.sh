2a4362441b457359c87ff88060e28c752e46ce67
shuchitasingh <singhshuchita5@gmail.com>
2021-03-19 19:28:59 +0530

    employee modified StoreDailyWages

--- shellscriptProgram.sh
+++ shellscriptProgram.sh
@@ -135,11 +135,11 @@ ISPARTTIME=2
 WORKINGDAYS=20
 WORK_HR_IN_MONTH=100
 
-#VARIABLE
+
 totalEmpHr=0
 totalWorkingDays=0
 
-#FUNCTIONS TO CHECK WORKING HOURS
+
 function getWorkHours() {
 	case $1 in
 		$ISFULLTIME)
@@ -171,4 +171,53 @@ echo "Total Salary is $totalSalary"
 
 
 
+WAGE_PER_HR=20
+ISFULLTIME=1
+ISPARTTIME=2
+WORKINGDAYS=20
+WORK_HR_IN_MONTH=100
+
+
+totalEmpHr=0
+totalWorkingDays=0
+
+
+function getWorkHours() {
+	case $1 in
+		$ISFULLTIME)
+			empHrs=8
+			;;
+		$ISPARTTIME)
+			empHrs=4
+			;;
+		*)
+			empHrs=0
+			;;
+	esac
+	echo $empHrs
+}
+
+
+function calculateDailyWage() {
+local empHrs=$1
+wage=$(($empHrs*$WAGE_PER_HR))
+echo $wage
+}
+
+
+while [[ $totalWorkingHr -lt $WORK_HR_IN_MONTH &&
+         $totalWorkingDays -lt $WORKINGDAYS ]]
+do
+	randomCheck=$((RANDOM%3))
+	((totalWorkingDays++))
+	empHrs=$( getWorkHours $randomCheck )
+	totalWorkingHr=$(($totalWorkingHr+$empHrs))
+
+
+	dailyWage[$totalWorkingDays]=$( calculateDailyWage $empHrs )
+done
+totalSalary=$(($totalWorkingHr*$WAGE_PER_HR))
+echo "Total Working employee hr in a month is $totalWorkingHr"
+echo "Total Salary is $totalSalary"
+echo "Daily Wage is ${dailyWage[@]} "
 
