99a249001e7a0e437a276eac5201855d8f03411d
shuchitasingh <singhshuchita5@gmail.com>
2021-03-19 18:55:57 +0530

    TOGetWorkHr

--- shellscriptProgram.sh
+++ shellscriptProgram.sh
@@ -126,3 +126,49 @@ done
 totalSalary=$(($totalEmpHr*$WAGE_PER_HR))
 echo "Total employee hr in a month is $totalEmpHr"
 echo "Total Salary is $totalSalary"
+
+
+
+WAGE_PER_HR=20
+ISFULLTIME=1
+ISPARTTIME=2
+WORKINGDAYS=20
+WORK_HR_IN_MONTH=100
+
+#VARIABLE
+totalEmpHr=0
+totalWorkingDays=0
+
+#FUNCTIONS TO CHECK WORKING HOURS
+function getWorkHours() {
+	case $1 in
+		$ISFULLTIME)
+			empHrs=8
+			;;
+		$ISPARTTIME)
+			empHrs=4
+			;;
+		*)
+			empHrs=0
+			;;
+	esac
+	echo $empHrs
+}
+
+
+while [[ $totalWorkingHr -lt $WORK_HR_IN_MONTH &&
+         $totalWorkingDays -lt $WORKINGDAYS ]]
+do
+	randomCheck=$((RANDOM%3))
+	((totalWorkingDays++))
+	empHrs=$( getWorkHours $randomCheck )
+	totalWorkingHr=$(($totalWorkingHr+$empHrs))
+done
+totalSalary=$(($totalWorkingHr*$WAGE_PER_HR))
+echo "Total Working employee hr in a month is $totalWorkingHr"
+echo "Total Salary is $totalSalary"
+
+
+
+
+
